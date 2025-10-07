5d5898e6710ef4f2e9dd0a96396b3c296e23680e
denizmetn <dniizmetiiin@gmail.com>
2025-10-07 18:52:17 +0300

    Add full project files

---
+++ src/main/java/com/deniz/bloomlishbackend/BloomlishBackendApplication.java
@@ -0,0 +1,13 @@
+package com.deniz.bloomlishbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloomlishBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BloomlishBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/deniz/bloomlishbackend/BloomlishBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deniz.bloomlishbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloomlishBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
