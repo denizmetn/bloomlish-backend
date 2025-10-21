9d75ddf6c480e40251163750e23c925ad55609b5
zeynepccn <114948724+zeynepccn@users.noreply.github.com>
2025-10-21 16:22:24 +0300

    arrangement done

--- src/main/java/com/deniz/bloomlishbackend/mapper/BlogPostMapper.java
+++ src/main/java/com/deniz/bloomlishbackend/mapper/BlogPostMapper.java
@@ -8,7 +8,7 @@ import org.mapstruct.Mapper;
 import java.util.List;
 
 @Mapper(componentModel = "spring")
-public interface BlogPostMapper {
+public interface  BlogPostMapper {
 
     CommentDto commentToDto(Comment comment);
     Comment dtoToComment(CommentDto dto);
